7d6949e88ae28a1e00d6f28f31f92092a003c420
Kristen <kristenwood123@gmail.com>
2021-04-03 16:26:01 -0700

    added js setup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-  baguetteBox.run('.gallery');
+
+let input = document.getElementById('input')
+const handleSearch = (e) => {
+  console.log(e.target.value)
+}
+
+input.addEventListener('keyup', handleSearch)
+
+
+
+baguetteBox.run('.gallery', {
+    captions: true,
+    buttons: 'auto',
+    animation: 'fadeIn'
+});
