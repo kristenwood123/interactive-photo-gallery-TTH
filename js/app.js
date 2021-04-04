4720ff64129b55c111ec263567190945ada9bcf9
Kristen <kristenwood123@gmail.com>
2021-04-04 12:09:03 -0700

    added js logic

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 
 let input = document.getElementById('input')
+
 const handleSearch = (e) => {
-  console.log(e.target.value)
+  let inputValue = e.target.value.toLowerCase();
+    let array = document.getElementsByClassName('item')
+      let caption = array[i].getAttribute('data-caption')
+     if(caption.includes(inputValue)) {
+      console.log(array[i]);
+  for (let i = 0; i < array.length; i++) {
+    }
+  }
 }
 
 input.addEventListener('keyup', handleSearch)
+
+
+  
+
 
 
 
