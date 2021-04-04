d8f286f24569e986847d2eee19d8d051d94dee09
Kristen <kristenwood123@gmail.com>
2021-04-04 12:28:10 -0700

    completed js logic

--- js/app.js
+++ js/app.js
@@ -3,22 +3,22 @@ let input = document.getElementById('input')
 
 const handleSearch = (e) => {
   let inputValue = e.target.value.toLowerCase();
-    let array = document.getElementsByClassName('item')
-      let caption = array[i].getAttribute('data-caption')
-     if(caption.includes(inputValue)) {
-      console.log(array[i]);
+  let array = document.getElementsByClassName('item')
+
+
   for (let i = 0; i < array.length; i++) {
+    let caption = array[i].getAttribute('data-caption')
+    let alt = array[i].getAttribute('alt')
+    let image = array[i]
+     if(caption.includes(inputValue) || alt.includes(inputValue)) {
+      image.style.display = 'inline'
+    } else {
+      image.style.display = 'none'
     }
   }
 }
 
 input.addEventListener('keyup', handleSearch)
-
-
-  
-
-
-
 
 baguetteBox.run('.gallery', {
     captions: true,
